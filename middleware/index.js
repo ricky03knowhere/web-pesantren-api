06b7a8c18cc2c38fcf73044c21c6d0baeb76528e
Ricky Ackerman <67403273+ricky03knowhere@users.noreply.github.com>
2023-06-27 22:30:56 +0700

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,7 @@
 const multer = require("multer");
-const { join } = require('path');
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, join(__dirname, 'tmp')),
+  destination: (req, file, cb) => cb(null, 'tmp'),
   filename: (req, file, cb) =>
     cb(
       null,
